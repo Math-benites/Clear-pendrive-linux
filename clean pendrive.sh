5221bf11c7da215fe3cb6ee6a8aeeff1ee7b8c3f
Math-benites <32637272+Math-benites@users.noreply.github.com>
2018-03-27 13:10:14 -0300

    Update clean pendrive.sh

--- clean pendrive.sh
+++ clean pendrive.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 lsblk -a --output NAME,SIZE,TYPE
-echo Digite o caminho final da unidade /dev/...
+W='\033[1;33m'
+NC='\033[0m'
+printf "Digite o caminho final da unidade ${W}/dev/...${NC} \n"
 read sdb
 umount /dev/$sdb
 sudo  mkdosfs -F 32 -I /dev/$sdb
-
